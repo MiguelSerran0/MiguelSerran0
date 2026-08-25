2ac87d3c15a25d4c25d2f63fe6c516d283838a6d
Miguelserrano <migueldelosangeles30@gmail.com>
2026-08-25 17:58:34 -0600

    base path

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
 
-  base: '/',
+  base: '/MiguelSerran0/',
 
   server: {
     port: 5173,
